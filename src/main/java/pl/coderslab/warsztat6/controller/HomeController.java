5cf2046db0e9b256e75138b7b2384a2338dbfe15
Michał Tarkowski <tarski9@gmail.com>
2018-03-23 15:48:29 +0100

    Add User entity. Add user view

--- src/main/java/pl/coderslab/warsztat6/controller/HomeController.java
+++ src/main/java/pl/coderslab/warsztat6/controller/HomeController.java
@@ -2,14 +2,12 @@ package pl.coderslab.warsztat6.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
 	@GetMapping
-	@ResponseBody
 	public String test() {
-		return "test";
+		return "home";
 	}
 }
